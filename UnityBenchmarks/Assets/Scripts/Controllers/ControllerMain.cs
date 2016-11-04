1901739c1c1fa2117895162dec3b69baf3a0309e
orion <orion@play-i.com>
2016-11-04 10:54:10 -0700

    basic benchmark around vector math

---
+++ UnityBenchmarks/Assets/Scripts/Controllers/ControllerMain.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ControllerMain : MonoBehaviour {
+
+
+  public Text   output;
+  public Button btnCompareVectorMath;
+
+  void Start() {
+
+    btnCompareVectorMath.onClick.AddListener(onClickCompareVectorMath);
+
+    outputClear();
+    outputLine ("howdy");
+  }
+
+  void outputClear() {
+    output.text = "";
+  }
+
+  void outputLine(string line) {
+    if (output.text != "") {
+      line = "\n" + line;
+    }
+
+    outputString(line);
+  }
+
+  void outputString(string s) {
+    output.text += s;
+  }
+
+
+  void onClickCompareVectorMath() {
+    StartCoroutine(crCompareVectorMath());
+  }
+
+  IEnumerator crCompareVectorMath() {
+    outputLine("starting test CompareVectorMath");
+    yield return null;
+
+    int numIters = 30000000;
+
+    float[] scalars = new float[numIters];
+    for (int n = 0; n < numIters; ++n) {
+      scalars[n] = Random.Range(1f, 2f);
+    }
+
+    Vector3 vecStart = new Vector3(1f, 2f, 3f);
+
+    Vector3 vec;
+    float   s;
+
+    outputString(".");
+    yield return null;
+
+    vec = vecStart;
+    float t1a = Time.realtimeSinceStartup;
+    for (int n = 0; n < numIters; ++n) {
+      s    = scalars[n];
+      vec *= s;
+    }
+    float t1b = Time.realtimeSinceStartup;
+
+    outputString(".");
+    yield return null;
+
+    vec = vecStart;
+    float t2a = Time.realtimeSinceStartup;
+    for (int n = 0; n < numIters; ++n) {
+      s     = scalars[n];
+      vec.x = vec.x * s;
+      vec.y = vec.y * s;
+      vec.z = vec.z * s;
+    }
+    float t2b = Time.realtimeSinceStartup;
+
+
+    outputString(".");
+    yield return null;
+
+    vec = vecStart;
+    float t3a = Time.realtimeSinceStartup;
+    for (int n = 0; n < numIters; ++n) {
+      s      = scalars[n];
+      vec.x *= s;
+      vec.y *= s;
+      vec.z *= s;
+    }
+    float t3b = Time.realtimeSinceStartup;
+
+    float t1 = t1b - t1a;
+    float t2 = t2b - t2a;
+    float t3 = t3b - t3a;
+
+    float opsPerS1 = numIters / t1;
+    float opsPerS2 = numIters / t2;
+    float opsPerS3 = numIters / t3;
+
+    outputLine("operations per second, with " + numIters + " iterations.");
+    outputLine("method 1: " + (int)opsPerS1);
+    outputLine("method 2: " + (int)opsPerS2);
+    outputLine("method 3: " + (int)opsPerS3);
+
+    /*
+    method 1: 28232912
+    method 2: 64924828
+    method 3: 62071628
+    */
+
+  }
+
+}
